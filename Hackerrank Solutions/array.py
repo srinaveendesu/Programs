783ed274a0869b73cf86a921b1d37b5c22b702a6
srinaveendesu <srinaveen.desu@gmail.com>
2019-05-21 15:43:04 +0530

    Arrays in hackerrank - sparse matrix

--- Hackerrank Solutions/array.py
+++ Hackerrank Solutions/array.py
@@ -23,3 +23,59 @@ if __name__ == '__main__':
 
     print (' '.join(map(str,l)))
 
+
+#QQ# https://www.hackerrank.com/challenges/sparse-arrays/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    d = {}
+    for val in strings:
+        if val not in d.keys():
+            d[val] = 1
+        else:
+            d[val] = d[val] +1
+    print (d)
+    lst = []
+
+    for val in queries:
+        if val in d.keys():
+            lst.append(d[val])
+        else:
+            lst.append(0)
+    return lst
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
