795941ff006063cab154744237c560757ce2d6d9
srinaveendesu <srinaveen.desu@gmail.com>
2019-08-08 00:26:34 +0530

    Udemy - Simple Decorator that accepts parameters

---
+++ PythonScripts/Parameter_decorator.py
@@ -0,0 +1,30 @@
+import functools
+
+user = {'username': 'jose123', 'access_level': 'user'}
+
+# notice the three functions defined one within another
+def user_has_permission(access_level):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def secure_func(panel):
+            if user.get('access_level') == access_level:
+                return func(panel)
+        return secure_func
+    return my_decorator
+
+
+@user_has_permission('user')
+def my_function(panel):
+    """
+    Allows us to retrieve the password for the admin panel.
+    """
+    return f'Password for {panel} panel is 1234.'
+
+
+print(my_function.__name__)
+print(my_function('movies'))
+
+# Decorator unwrapping happens in the following way
+user_has_p = user_has_permission('user')
+my_func = user_has_p(my_function)
+print (my_func('games'))
