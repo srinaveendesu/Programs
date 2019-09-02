d94cb6e90417621277b7c9a1607f3a6b08c6afa8
desu srinaveen <srinaveen.desu@gmail.com>
2019-09-02 19:30:57 +0530

    Binary Search tree with recursion - Display Added

--- DataStructures and Algorithms/BinarySearchTree.py
+++ DataStructures and Algorithms/BinarySearchTree.py
@@ -182,6 +182,59 @@ class Tree():
         else:
             return treeLines
 
+    # Another implementation
+    # Taken from StackOverflow
+    def display(self, node):
+        lines, _, _, _ = self._display_aux(node)
+        for line in lines:
+            print(line)
+        return ''
+
+    def _display_aux(self, node):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if node.right is None and node.left is None:
+            line = '%s' % node.data
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if node.right is None:
+            lines, n, p, x = self._display_aux(node.left)
+            s = '%s' % node.data
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if node.left is None:
+            lines, n, p, x = self._display_aux(node.right)
+            s = '%s' % node.data
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self._display_aux(node.left)
+        right, m, q, y = self._display_aux(node.right)
+        s = '%s' % node.data
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
     def __str__(self):
         tree_output = self.printBTree(self.root, lambda node: (str(node.data), node.left, node.right),inverted=0)
         return '\n'.join(tree_output)
@@ -224,4 +277,12 @@ print(BST.inorder(BST.root))
 print((BST.postorder(BST.root)))
 # [1, 4, 7, 6, 3, 13, 14, 10, 8]
 
-print(BST.root)
+
+print(BST.display(BST.root))
+#   ___8_
+#  /     \
+#  3_   10___
+# /  \       \
+# 1  6      14
+#   / \    /
+#   4 7   13
