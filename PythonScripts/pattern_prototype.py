46321a85c3f46ba4c83a095268c13fea7bd9b732
desu srinaveen <srinaveen.desu@gmail.com>
2019-10-02 01:46:56 +0530

    Creational Pattern - Prototype

---
+++ PythonScripts/pattern_prototype.py
@@ -0,0 +1,48 @@
+# Creational Pattern
+# Prototype clones objects according to a prototypical instance
+
+# Useful when creating identical objects : Expensive
+# Cloning : Alternative solution for above problem
+
+# Pattern related to is the Abstract pattern
+
+import copy
+
+
+class Prototype:
+
+    def __init__(self):
+        self._objects = {}
+
+    def register_object(self, name, obj):
+        """Register an object"""
+        self._objects[name] = obj
+
+    def unregister_object(self, name):
+        """Unregister an object"""
+        del self._objects[name]
+
+    def clone(self, name, **attr):
+        """Clone a registered object and update its attributes"""
+        obj = copy.deepcopy(self._objects.get(name))
+        obj.__dict__.update(attr)
+        return obj
+
+
+class Car:
+    def __init__(self):
+        self.name = "Benz"
+        self.color = "Red"
+        self.options = "Ex"
+
+    def __str__(self):
+        return '{} | {} | {}'.format(self.name, self.color, self.options)
+
+
+c = Car()
+prototype = Prototype()
+prototype.register_object('Benz', c)
+
+c1 = prototype.clone('Benz')
+
+print(c1)
