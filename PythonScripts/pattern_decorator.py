077c74e425ffc4f2848129f95ffa3551439cc417
desu srinaveen <srinaveen.desu@gmail.com>
2019-10-04 16:34:06 +0530

    Structural Pattern - Decorator Pattern

--- PythonScripts/pattern_decorator.py
+++ PythonScripts/pattern_decorator.py
@@ -1,11 +1,17 @@
 # Structural Pattern
 
-# Adding new features to existing object dynamically
-# and not using subclassing
-
-# Use functions acitng as objects in python feature
-
-# patterns such as adapter,composite and strategy are related to this pattern
+# Python makes implementing Decorator very straightforward due
+# to its built in language features. Our challenge here is to
+# add additional features to an existing object dynamically
+# without using subclassing. Here's our scenario. We start with
+# a function simply displaying a Hello World! message, and then
+# we'd like to make the message look fancier by decorating it with
+# additional tags, such as blink, as you can see here. Functions are
+# also objects in Python, and we can simply add additional features
+# to these functions using this built in decorator feature in Python.
+#
+#
+# Patterns such as adapter, composite, and strategy are related to the decorator pattern.
 
 from functools import wraps
 
