66493925b47c796553fc2d1f7ebac1cb192822bb
desu srinaveen <srinaveen.desu@gmail.com>
2019-10-04 16:23:35 +0530

    Creational Pattern - Singleton pattern

--- PythonScripts/pattern_singleton.py
+++ PythonScripts/pattern_singleton.py
@@ -1,7 +1,17 @@
 # Creational Pattern
-# When only one object to be instantiated for a class
 
-# Borg- Design pattern is used to implement Singleton
+# An object-oriented way of providing global variables is Singleton.
+# Singleton is the pattern you need when you'd like to allow only
+# one object to be instantiated from a class. Like I said, creating
+# a global variable in an object-oriented way is Singleton. Why would
+# you need such a pattern? Let's say that there is a need for keeping
+# a cache of information to be shared by various elements of your software
+# system. By keeping this information in the single object, there's no need
+# to retrieve the information from its original source every time. So in this case,
+# Singleton acts as a cache of the information. Modules in Python act as Singletons.
+#
+# There are also a number of ways of implementing Singleton,
+# but we'll be using the Borg design pattern to implement our Singleton.
 
 class Borg:
     """Borg pattern making the class attributes global"""
