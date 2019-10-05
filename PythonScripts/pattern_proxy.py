dd72fade46b757399615293618415b5430579134
desu srinaveen <srinaveen.desu@gmail.com>
2019-10-05 12:54:28 +0530

    Structural Pattern - Proxy Pattern

--- PythonScripts/pattern_proxy.py
+++ PythonScripts/pattern_proxy.py
@@ -1,11 +1,21 @@
 # Structural Pattern
 
-# Create an object which is resource intensive
-
-# Postpone  object creation unless absolutely necessary
-# find a placeholder for creation
-
-# Adapter and decorator are related to proxy pattern
+# Proxy becomes handy when creating an object that is very
+# resource intensive. The problem we need to solve here is to
+# postpone our object creation as long as possible, due to the
+# resource intensive nature of the object we're creating. Therefore,
+# there is a need for a placeholder which will in turn create the
+# object if its creation is absolutely necessary. So here is our scenario.
+# So in our scenario, we create this instance of the Producer class
+# only when he's available, because only a fixed number of Producer
+# objects can exist at a given time. Our Proxy in this case is an artist
+# who is checking to see if the Producer becomes available for a guest.
+# In the Proxy design pattern clients interact with a Proxy object most
+# of the time until the resource intensive object becomes available.
+# Therefore, the Proxy object is in charge of creating the resource intensive objects.
+#
+#
+# Adapter and Decorator are related to the Proxy design pattern.
 
 import time
 
