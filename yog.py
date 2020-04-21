217473e45672c41ebb205dd1c3247bef7bf7a812
Desu Srinaveen <s.desu@f5.com>
2020-04-21 20:22:09 +0530

    daily regression

--- yog.py
+++ yog.py
@@ -590,8 +590,64 @@ print(data)
 #                   Best              Average           Worst           Space
 # Counting Sort	    Ω(n+k)	          θ(n+k)	        O(n+k)          o(k)
 
+# A function to do counting sort of arr[] according to
+# the digit represented by exp.
+def radixcountingSort(arr, exp1):
+    n = len(arr)
 
-# Radix Sort	Ω(nk)	θ(nk)	O(nk)
+    # The output array elements that will have sorted arr
+    output = [0] * (n)
+
+    # initialize count array as 0
+    count = [0] * (10)
+
+    # Store count of occurrences in count[]
+    for i in range(0, n):
+        index = (arr[i] // exp1) %10
+        count[index] += 1
+
+    # Change count[i] so that count[i] now contains actual
+    #  position of this digit in output array
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    # Build the output array
+    i = n - 1
+    while i >= 0:
+        index = (arr[i] // exp1) % 10
+        output[count[index ] - 1] = arr[i]
+        count[index] -= 1
+        i -= 1
+
+    # Copying the output array to arr[],
+    # so that arr now contains sorted numbers
+    i = 0
+    for i in range(0, len(arr)):
+        arr[i] = output[i]
+
+    # Method to do Radix Sort
+
+
+def radixSort(arr):
+    # Find the maximum number to know number of digits
+    max1 = max(arr)
+
+    # Do counting sort for every digit. Note that instead
+    # of passing digit number, exp is passed. exp is 10^i
+    # where i is current digit number
+    exp = 1
+    while max1 // exp > 0:
+        radixcountingSort(arr, exp)
+        exp *= 10
+
+
+# Driver code to test above
+arr = [170, 45, 75, 90, 802, 24, 2, 66]
+radixSort(arr)
+print(arr)
+
+#                   Best              Average           Worst           Space
+# Radix Sort	    Ω(nk)	          θ(nk)	            O(nk)           o(n+k)
 
 
 
