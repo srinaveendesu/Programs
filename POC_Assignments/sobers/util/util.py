a88b471d8c169e8d56932f53cf18a8d07c1dc0a7
Desu Srinaveen <s.desu@f5.com>
2020-06-21 15:54:34 +0530

    sobers

---
+++ POC_Assignments/sobers/util/util.py
@@ -0,0 +1,69 @@
+import os
+import pandas as pd
+from pandas.core.dtypes.generic import ABCDataFrame
+
+__all__ = ['CsvMerger','JsonMerger','XmlMerger','DataPreprocessor']
+
+class Processor:
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
+
+    def process(self):
+        data = self.reader.read()
+        if isinstance(data, ABCDataFrame):
+            if len(data) ==0: return
+        else:
+            if not data: return
+        data = self.converter(data)
+        self.writer.write(data)
+
+    def converter(self, data):
+        raise NotImplementedError('converter must be defined!')
+
+class DataPreprocessor(Processor):
+    def converter(self, dataFrame):
+        df = dataFrame.apply(lambda col: pd.to_datetime(col, errors='ignore') if col.dtypes == object else col, axis=0)
+        columns = ['from', 'to']
+        for col in df.select_dtypes(include=['datetime', 'object']):
+            columns.insert(0, col)
+        df2 = df[df.columns.difference(columns, sort=False)]
+        merge_colums = list(df2.columns)
+        try:
+            df2['amount'] = df2[merge_colums[0]].map(str) + '.' + df2[merge_colums[1]].map(str)
+            df2['amount'] = df2['amount'].astype(float)
+            df2['amount'] = df2['amount'].round(decimals=2)
+        except:
+            pass
+        try:
+            df3 = pd.concat([df[columns], df2['amount']], axis=1).reindex(df.index)
+        except:
+            df3 = pd.concat([df[columns], df2[list(df2.columns)[0]]], axis=1).reindex(df.index)
+        df3 = df3.rename({list(df.select_dtypes(include=['object']))[0]: 'transaction'}, axis='columns')
+        df3 = df3.rename({list(df.select_dtypes(include=['datetime']))[0]: 'date'}, axis='columns')
+
+        return df3
+
+class CsvMerger():
+    def __init__(self,readfilename=None,writefilename=None):
+        self.readfilename = readfilename
+        self.writefilename = writefilename
+
+    def read(self):
+        self.dataframe = pd.read_csv(self.readfilename, parse_dates=True, header=0)
+        return self.dataframe
+
+    def write(self,dataframe):
+        if not os.path.isfile(self.writefilename):
+            dataframe.to_csv(self.writefilename, index=False)
+        else:  # else it exists so append without writing the header
+            dataframe.to_csv(self.writefilename, mode='a', header=False, index=False)
+
+class JsonMerger(Processor):
+    pass
+
+class XmlMerger(Processor):
+    pass
+
+
+class PrivateExc(Exception): pass
