0fdb2892596189e9effb1d040c3f3922bd29bce0
Desu Srinaveen <s.desu@f5.com>
2020-07-03 11:54:38 +0530

    leet code - Longest Palindromic Substring

--- leetcode/longest_palindrome.py
+++ leetcode/longest_palindrome.py
@@ -47,6 +47,20 @@ start = datetime.datetime.now()
 longestPalindrome(ss)
 print(datetime.datetime.now() - start)
 
+
+# brute force approach 2
+
+def longestPalindrome( s: str) -> str:
+    m = ''  # Memory to remember a palindrome
+    for i in range(len(s)):  # i = start, O = n
+        for j in range(len(s), i, -1):  # j = end, O = n^2
+            if len(m) >= j - i:  # To reduce time
+                break
+            elif s[i:j] == s[i:j][::-1]:
+                m = s[i:j]
+                break
+    return m
+
 # manacher's algorithm implementation
 
 class Solution:
