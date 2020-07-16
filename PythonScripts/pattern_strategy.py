28238c976e35562600be2d04d899fdb33dc04519
Desu Srinaveen <s.desu@f5.com>
2020-07-17 01:30:43 +0530

    Behavioral Design pattern - Strategy

--- PythonScripts/pattern_strategy.py
+++ PythonScripts/pattern_strategy.py
@@ -57,3 +57,102 @@ s2.execute()
 # Default Strategy is used!
 # Strategy One is used to execute method 1
 # Strategy Two is used to execute method 2
+
+# Example2
+
+# he Strategy pattern allows you to define a family of algorithms, encapsulate each one, and makes them interchangeable.
+# It lets the algorithm vary independently from the clients that use it
+
+class PrimeFinder:
+
+    def __init__(self):
+        self.primes = []
+
+    def calculate(self, limit):
+        """ Will calculate all the primes below limit. """
+        pass
+
+    def out(self):
+        """ Prints the list of primes prefixed with which algorithm made it """
+        print(self.__class__.__name__)
+        for prime in self.primes:
+            print(prime)
+
+
+class HardCodedPrimeFinder(PrimeFinder):
+    def calculate(self, limit):
+        hardcoded_primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+        primes = []
+        for prime in hardcoded_primes:
+            if (prime < limit):
+                self.primes.append(prime)
+
+
+class StandardPrimeFinder(PrimeFinder):
+    def calculate(self, limit):
+        self.primes = [2]
+        # check only odd numbers.
+        for number in range(3, limit, 2):
+            is_prime = True
+            for prime in self.primes:
+                if (number % prime == 0):
+                    is_prime = False
+                    break
+            if (is_prime):
+                self.primes.append(number)
+
+
+class PrimeFinderClient:
+
+    def __init__(self, limit):
+        self.limit = limit
+        if limit <= 50:
+            self.finder = HardCodedPrimeFinder()
+        else:
+            self.finder = StandardPrimeFinder()
+
+    def get_primes(self):
+        self.finder.calculate(self.limit)
+        self.finder.out()
+
+p = PrimeFinderClient(50)
+p.get_primes()
+
+p = PrimeFinderClient(65)
+p.get_primes()
+
+# HardCodedPrimeFinder
+# 2
+# 3
+# 5
+# 7
+# 11
+# 13
+# 17
+# 19
+# 23
+# 29
+# 31
+# 37
+# 41
+# 43
+# 47
+# StandardPrimeFinder
+# 2
+# 3
+# 5
+# 7
+# 11
+# 13
+# 17
+# 19
+# 23
+# 29
+# 31
+# 37
+# 41
+# 43
+# 47
+# 53
+# 59
+# 61
