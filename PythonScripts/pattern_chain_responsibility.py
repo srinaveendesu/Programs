fd572c4eb218618efe4c192f3a7d05c7f535bf05
Desu Srinaveen <s.desu@f5.com>
2020-07-17 01:12:24 +0530

    Behavioral Design pattern - Chain of responsibility

--- PythonScripts/pattern_chain_responsibility.py
+++ PythonScripts/pattern_chain_responsibility.py
@@ -68,3 +68,77 @@ c.delegate(requests)
 # Request 2 handled in handler 1
 # Request 5 handled in handler 1
 # End of chain, no handler for 30
+
+# Example 2
+
+# he Chain of Responsibility pattern avoids coupling the sender of a request to the receiver,
+# by giving more than one object a chance to handle that request
+
+class Car:
+	def __init__(self, name, water, fuel, oil):
+		self.name = name
+		self.water = water
+		self.fuel = fuel
+		self.oil = oil
+
+	def is_fine(self):
+		if self.water >= 20 and self.fuel >= 5 and self.oil >= 10:
+			print('Car is good to go')
+			return True
+		else:
+			return False
+
+
+class Handler:
+	def __init__(self, successor=None):
+		self._successor = successor
+
+	def handle_request(self, car):
+		if not car.is_fine() and self._successor is not None:
+			self._successor.handle_request(car)
+
+
+class WaterHandler(Handler):
+
+	def handle_request(self, car):
+		if car.water < 20:
+			car.water = 100
+			print('Added water')
+		super().handle_request(car)
+
+
+class FuelHandler(Handler):
+
+	def handle_request(self, car):
+		if car.fuel < 5:
+			car.fuel = 100
+			print('Added fuel')
+		super().handle_request(car)
+
+
+class OilHandler(Handler):
+
+	def handle_request(self, car):
+		if car.oil < 10:
+			car.oil = 100
+			print('Added oil')
+		super().handle_request(car)
+
+
+garage_handler = OilHandler(FuelHandler(WaterHandler()))
+car = Car('mycar',1,1,1)
+garage_handler.handle_request(car)
+car = Car('mycar',5,5,5)
+garage_handler.handle_request(car)
+car = Car('mycar',10,10,10)
+garage_handler.handle_request(car)
+
+# Added oil
+# Added fuel
+# Added water
+# Car is good to go
+# Added oil
+# Added water
+# Car is good to go
+# Added water
+# Car is good to go
