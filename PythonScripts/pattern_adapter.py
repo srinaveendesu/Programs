9cdf16eaf5f6aa85ff8a688976e07416bcdfab19
Desu Srinaveen <s.desu@f5.com>
2020-07-16 20:05:53 +0530

    Structural Design pattern - Adapter

--- PythonScripts/pattern_adapter.py
+++ PythonScripts/pattern_adapter.py
@@ -67,3 +67,78 @@ for obj in objects:
 # Korean says 'An-neyong?'
 #
 # British says 'Hello!'
+
+
+# Example 2
+
+# The adapter converts the interface of a class into another interface that clients expect.
+# It lets classes work together that couldn't otherwise, because of incompatible interfaces
+
+# Adaptee (source) interface
+class EuropeanSocketInterface:
+    def voltage(self): pass
+    def live(self): pass
+    def neutral(self): pass
+    def earth(self): pass
+
+# Target interface
+class USASocketInterface:
+    def voltage(self): pass
+    def live(self): pass
+    def neutral(self): pass
+
+# Adaptee
+class EuropeanSocket(EuropeanSocketInterface):
+    def voltage(self):
+        return 230
+
+    def live(self):
+        return 1
+
+    def neutral(self):
+        return -1
+
+    def earth(self):
+        return 0
+
+# Client
+class AmericanKettle:
+    __power = None
+
+    def __init__(self, power):
+        self.__power = power
+
+    def boil(self):
+        if self.__power.voltage() > 110:
+            print("Kettle on fire!")
+        else:
+            if self.__power.live() == 1 and self.__power.neutral() == -1:
+                print("Coffee time!")
+            else:
+                print("No power.")
+
+class Adapter(USASocketInterface):
+    __socket = None
+
+    def __init__(self, socket):
+        self.__socket = socket
+
+    def voltage(self):
+        return 110
+
+    def live(self):
+        return self.__socket.live()
+
+    def neutral(self):
+        return self.__socket.neutral()
+
+socket = EuropeanSocket()
+kettle = AmericanKettle(socket)
+kettle.boil()
+
+adapter = Adapter(socket)
+kettle = AmericanKettle(adapter)
+kettle.boil()
+
+# Kettle on fire!
+# Coffee time!
