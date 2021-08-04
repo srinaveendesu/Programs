ef4b14a4e12ebd1e5a49271223d32fa99a9b510a
Srinaveen Desu <srinaveen.desu@gmail.com>
2021-08-04 21:49:12 +0530

    day 27 - Aug 4 - 1 probs

---
+++ leetcode/day27.py
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+# SELECT
+# class
+#     FROM
+# (SELECT
+# class , COUNT(DISTINCT student) AS num
+# FROM
+# courses
+# GROUP
+# BY
+# class ) AS temp_table
+# WHERE
+# num >= 5
+# ;
