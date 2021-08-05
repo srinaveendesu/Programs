fd14119a18a3ca1355a91faf4a4955d31cb5e91e
Srinaveen Desu <srinaveen.desu@gmail.com>
2021-08-05 23:31:49 +0530

    day 28 - Aug 5 - 1 probs

---
+++ leetcode/day28.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/not-boring-movies/submissions/
+
+# Write your MySQL query statement below
+
+# select *
+# from cinema
+# where mod(id, 2) = 1 and description != 'boring'
+# order by rating DESC
+# ;
